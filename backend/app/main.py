e8019eb9dd94f0a17b53eee71b07f14fc9b824c6
Madhesh39 <madhesh39@users.noreply.github.com>
2026-08-12 23:41:18 +0530

    Add Render Blueprint render.yaml and production CORS & API URL configs

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,11 +19,12 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "*"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
